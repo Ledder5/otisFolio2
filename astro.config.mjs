c50dbced3c50acd4beae9f810536887032c46c2d
Jude Robinson <laughingsquid369@gmail.com>
2026-06-23 17:36:40 +0100

    Changed over to local icons (non working)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
-
+import { visualizer } from 'rollup-plugin-visualizer';
 import tailwindcss from '@tailwindcss/vite';
-
 import icon from 'astro-icon';
 
 // https://astro.build/config
@@ -11,12 +9,18 @@ export default defineConfig({
   // site: '#',
   // base: '/',
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [
+      tailwindcss(),
+      visualizer({
+        filename: 'bundle-stats.html',
+        template: 'treemap'
+      })
+    ]
   },
 
   integrations: [icon()],
 
   adapter: cloudflare(),
-  
+
   output: 'server' // or 'static'
 });
