c7cb9e572b29a3dff63e1cb533be536125999efa
Jude Robinson <laughingsquid369@gmail.com>
2026-06-25 19:33:17 +0100

    changed colour scheme

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const SITE_CONFIG = {
     // ----- THEME VARIABLE -----
     // FEEL FREE TO CHANGE THIS TO ONE OF THE OPTIONS
-    // Options: 'default', 'strategic', 'innovator', 'executive'
-  baseTheme: 'executive',
+    // Options: 'default', 'strategic', 'innovator', 'executive', 'blaze', 'mint', 'solar', 'monochrome', 'quantum'
+  baseTheme: 'blaze',
 };
